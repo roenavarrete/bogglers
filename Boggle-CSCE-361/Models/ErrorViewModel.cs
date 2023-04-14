2fa0b4075db2ef154dccf5966a446ccd734012de
Connor Nichols <32244381+connor-nichols@users.noreply.github.com>
2023-04-14 12:20:20 -0500

    init

---
+++ Boggle-CSCE-361/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Boggle_CSCE_361.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
